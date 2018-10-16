96eb1e489a60ef36c2c6fc860e3f0e9bc91dbac2
MrSimbax <matt@przybyl.io>
2018-10-16 20:47:12 +0200

    Add a note about custom type logs

--- src/misc/custom_log.h
+++ src/misc/custom_log.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <vector>
 
 #include <plog/Log.h>
@@ -7,15 +8,35 @@
 namespace plog
 {
 
+// Note: if using plog version <=1.1.4, use `LOG << "" << customObject;` (a string first) otherwise it won't compile
+
 template<typename T>
-Record& operator<<(Record& record, const std::vector<T>& vector)
+Record& operator<<(Record& record, const std::vector<T>& container)
 {
     record << "\n{";
-    for (const auto& value : vector)
+    const auto separator = ", ";
+    const auto* sep = "";
+    for (const auto& value : container)
     {
-        record << "    " << value << ",\n";
+        record << sep << value;
+        sep = separator;
     }
-    record << "}\n";
+    record << "}";
+    return record;
+}
+
+template<typename T, size_t Size>
+Record& operator<<(Record& record, const std::array<T, Size>& container)
+{
+    record << "\n{";
+    const auto separator = ", ";
+    const auto* sep = "";
+    for (const auto& value : container)
+    {
+        record << sep << value;
+        sep = separator;
+    }
+    record << "}";
     return record;
 }
 
